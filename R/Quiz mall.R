03a35aec38e2066b207500e8299187fedfedf2b3
Dmitri Peredera <dmitri.peredera@gmail.com>
2015-09-27 19:11:34 +0300

    Template for R quizzes.

---
+++ R/Quiz mall.R
@@ -0,0 +1,53 @@
+setwd("~/R Projects/R Programming/Quiz4")
+
+"Question 1"
+
+
+# Ans: 
+
+"Question 2"
+
+
+# Ans: 
+
+"Question 3"
+
+
+# Ans: 
+
+"Question 4"
+
+
+# Ans: 
+
+"Question 5"
+
+
+# Ans: 
+
+"Question 6"
+
+
+# Ans: 
+
+"Question 7"
+
+
+# Ans: 
+
+"Question 8"
+
+
+# Ans: 
+
+"Question 9"
+
+
+# Ans: 
+
+"Question 10"
+
+
+# Ans: 
+
+
